a6b6ca828a0219e76c47509634a435870eb06934
Ahmad Rizki <amrizjr@gmail.com>
2024-07-25 16:51:36 +0700

    add self query to ojk

--- retriever/retriever_ojk/retriever_ojk.py
+++ retriever/retriever_ojk/retriever_ojk.py
@@ -14,66 +14,26 @@ from langchain_core.vectorstores import VectorStore
 from langchain_core.language_models.base import BaseLanguageModel
 from langchain_core.embeddings import Embeddings
 
-
-metadata_field_info = [
-    AttributeInfo(
-        name="title",
-        description="The title of the document of regulation",
-        type="string",
-    ),
-    AttributeInfo(
-        name="sector",
-        description="The sector of the regulation",
-        type="string",
-    ),
-    AttributeInfo(
-        name="subsector",
-        description="The subsector of the regulation",
-        type="string",
-    ),
-    AttributeInfo(
-        name="regulation_type",
-        description="The type of the regulation",
-        type="string",
-    ),
-    AttributeInfo(
-        name="regulation_number",
-        description="The number of the regulation",
-        type="string",
-    ),
-    AttributeInfo(
-        name="effective_date",
-        description="The effective date of the regulation",
-        type="string",
-    ),
-]
-
-document_content_description = "The content of the document"
+from retriever.retriever_ojk.self_query_ojk import self_query_ojk
 
 
 def get_retriever_ojk(vector_store: VectorStore, llm_model: BaseLanguageModel, embed_model: Embeddings, top_n: int = 7, top_k:int = 20, config: dict = {}):
 
-    top_k = top_k // 2
+    top_k = top_k // 4
 
-    # retriever_self_query_similarity = SelfQueryRetriever.from_llm(
-    # llm=llm_model,
-    # vectorstore=vector_store,
-    # document_contents=document_content_description,
-    # metadata_field_info=metadata_field_info,
-    # enable_limit=False,
-    # search_type="similarity",
-    # search_kwargs={"k": top_k},
-    # )
-    
-    # retriever_self_query_mmr = SelfQueryRetriever.from_llm(
-    # llm=llm_model,
-    # vectorstore=vector_store,
-    # document_contents=document_content_description,
-    # metadata_field_info=metadata_field_info,
-    # enable_limit=False,
-    # search_type="mmr",
-    # search_kwargs={"k": top_k},
-    # )
+    retriever_self_query_similarity = self_query_ojk(
+        llm_model=llm_model, 
+        vector_store=vector_store, 
+        search_type="similarity", 
+        top_k=top_k
+    )
+
+    retriever_self_query_mmr = self_query_ojk(
+        llm_model=llm_model, 
+        vector_store=vector_store, 
+        search_type="mmr", 
+        top_k=top_k
+    )
 
     retriever_similarity = vector_store.as_retriever(
         search_type="similarity", 
@@ -85,7 +45,7 @@ def get_retriever_ojk(vector_store: VectorStore, llm_model: BaseLanguageModel, e
     )
 
     # merge retrievers
-    lotr = MergerRetriever(retrievers=[retriever_similarity, retriever_mmr])
+    lotr = MergerRetriever(retrievers=[retriever_self_query_similarity, retriever_self_query_mmr, retriever_similarity, retriever_mmr])
 
     # remove redundant documents
     filter = EmbeddingsRedundantFilter(embeddings=embed_model)
