7482bbf27909be56a7b497c35e895c3fe7dcbe1f
Ahmad Rizki <amrizjr@gmail.com>
2024-07-25 13:27:14 +0700

    add llm model name and embedding name in models

--- utils/models.py
+++ utils/models.py
@@ -12,39 +12,62 @@ class ModelName(Enum):
     OPENAI = 'openai'
     AZURE_OPENAI = 'azure_openai'
 
+class LLMModelName(Enum):
+    GPT_35_TURBO = 'gpt-3.5-turbo'
+
+class EmbeddingModelName(Enum):
+    EMBEDDING_ADA = 'ada'
+    EMBEDDING_3_SMALL = '3-small'
+
 # ================== FUNCTIONS ==================
 
 
-def get_openai_models(api_key: str):
-    llm = ChatOpenAI(
-        api_key=api_key,
-        temperature=0.0,
-        verbose=True,
-        model="gpt-3.5-turbo",
-    )
-    embedding_llm = OpenAIEmbeddings(
-        api_key=api_key,
-        # model="text-embedding-3-small"
-    )
+def get_openai_models(api_key: str, llm_model_name: LLMModelName, embedding_model_name: EmbeddingModelName):
+    if llm_model_name == LLMModelName.GPT_35_TURBO:
+        llm = ChatOpenAI(
+            api_key=api_key,
+            temperature=0.0,
+            verbose=True,
+            model="gpt-3.5-turbo",
+        )
+        
+    if embedding_model_name == EmbeddingModelName.EMBEDDING_ADA:
+        embedding_llm = OpenAIEmbeddings(
+            api_key=api_key,
+        )
+    elif embedding_model_name == EmbeddingModelName.EMBEDDING_3_SMALL:
+        embedding_llm = OpenAIEmbeddings(
+            api_key=api_key,
+            model="text-embedding-3-small"
+        )
     return llm, embedding_llm
 
 
-def get_azure_openai_models(azure_endpoint: str, azure_deployment: str, api_version: str, api_key: str):
-    llm = AzureChatOpenAI(
-        azure_endpoint=azure_endpoint,
-        azure_deployment=azure_deployment,
-        api_version=api_version,
-        api_key=api_key,
-        temperature=0.0,
-        verbose=True,
-    )
-    embedding_llm = AzureOpenAIEmbeddings(
-        azure_endpoint=azure_endpoint,
-        azure_deployment='embedding-ada-crayon',
-        # azure_deployment='embedding-3-small',
-        api_key=api_key,
-        api_version=api_version,
-    )
+def get_azure_openai_models(azure_endpoint: str, azure_deployment: str, api_version: str, api_key: str, llm_model_name: LLMModelName, embedding_model_name: EmbeddingModelName):
+    if llm_model_name == LLMModelName.GPT_35_TURBO:
+        llm = AzureChatOpenAI(
+            azure_endpoint=azure_endpoint,
+            azure_deployment=azure_deployment,
+            api_version=api_version,
+            api_key=api_key,
+            temperature=0.0,
+            verbose=True,
+        )
+    
+    if embedding_model_name == EmbeddingModelName.EMBEDDING_ADA:
+        embedding_llm = AzureOpenAIEmbeddings(
+            azure_endpoint=azure_endpoint,
+            azure_deployment='embedding-ada-crayon',
+            api_key=api_key,
+            api_version=api_version,
+        )
+    elif embedding_model_name == EmbeddingModelName.EMBEDDING_3_SMALL:
+        embedding_llm = AzureOpenAIEmbeddings(
+            azure_endpoint=azure_endpoint,
+            azure_deployment='embedding-3-small',
+            api_key=api_key,
+            api_version=api_version,
+        )
     return llm, embedding_llm
 
 
@@ -60,10 +83,10 @@ def get_ollama_models():
 # ================== MAIN ==================
 
 
-def get_model(model_name: ModelName, config: dict = {}):
+def get_model(model_name: ModelName, config: dict = {}, llm_model_name: LLMModelName = None, embedding_model_name: EmbeddingModelName = None):
     if model_name == ModelName.OPENAI:
-        return get_openai_models(**config['config_openai'])
+        return get_openai_models(**config['config_openai'], llm_model_name=llm_model_name, embedding_model_name=embedding_model_name)
     elif model_name == ModelName.AZURE_OPENAI:
-        return get_azure_openai_models(**config['config_azure'])
+        return get_azure_openai_models(**config['config_azure'], llm_model_name=llm_model_name, embedding_model_name=embedding_model_name)
     elif model_name == ModelName.OLLAMA:
         return get_ollama_models()
