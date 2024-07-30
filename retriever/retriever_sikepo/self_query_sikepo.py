34e0ddd23b266f8000a431bd0ad950a8096f1d4a
bryandts7 <bryandts7@gmail.com>
2024-07-30 17:26:16 +0700

    pivot to elastic

--- retriever/retriever_sikepo/self_query_sikepo.py
+++ retriever/retriever_sikepo/self_query_sikepo.py
@@ -2,6 +2,7 @@ from langchain.chains.query_constructor.schema import AttributeInfo
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain_core.language_models.base import BaseLanguageModel
 from langchain_core.vectorstores import VectorStore
+from langchain.retrievers.self_query.pgvector import PGVectorTranslator
 
 
 metadata_field_info = [
@@ -44,14 +45,32 @@ metadata_field_info = [
 
 document_content_description = "Isi Ketentuan dari Peraturan"
 
+from typing import Dict, Tuple, Union
+from langchain_core.structured_query import (
+    Comparator,
+    Comparison,
+    Operation,
+    Operator,
+    StructuredQuery,
+    Visitor,
+)
+from langchain.retrievers.self_query.pgvector import PGVectorTranslator
+
+class CustomPGVectorTranslator(PGVectorTranslator):
+    """Custom PGVectorTranslator to add `$` prefix to comparators."""
+
+    def _format_func(self, func: Union[Operator, Comparator]) -> str:
+        self._validate_func(func)
+        return f"${func.value}"  # Add the `$` prefix
 
 # ini juga gua buat satu aja jadi kalau mau make self self_query_retriever_ketentuan_terkait, tinggal pake vector_store ketentuan terkait aja
-def self_query_retriever_sikepo(llm_model: BaseLanguageModel, vector_store: VectorStore):
+def self_query_retriever_sikepo(llm_model: BaseLanguageModel, vector_store: VectorStore, with_limit: bool = False):
     retriever = SelfQueryRetriever.from_llm(
         llm=llm_model,
         vectorstore=vector_store,
         document_contents=document_content_description,
         metadata_field_info=metadata_field_info,
         verbose=True,
+        enable_limit=with_limit,
     )
     return retriever
